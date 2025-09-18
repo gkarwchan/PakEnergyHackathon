d8e7d6c79c7522b92e9349603a5255aa5e270290
Ghassan Karwchan <ghassan.karwchan@gmail.com>
2025-09-17 19:31:09 -0600

    clean solution

--- app/backend/Extensions/WebApplicationExtensions.cs
+++ app/backend/Extensions/WebApplicationExtensions.cs
@@ -8,10 +8,7 @@ internal static class WebApplicationExtensions
     {
         var api = app.MapGroup("api");
 
-        // Blazor 📎 Clippy streaming endpoint
-        api.MapPost("openai/chat", OnPostChatPromptAsync);
-
-        // Long-form chat w/ contextual history endpoint
+        // Chat prompt
         api.MapPost("chat", OnPostChatAsync);
 
         // Upload a document
@@ -20,55 +17,10 @@ internal static class WebApplicationExtensions
         // Get all documents
         api.MapGet("documents", OnGetDocumentsAsync);
 
-        // Get DALL-E image result from prompt
-        api.MapPost("images", OnPostImagePromptAsync);
-
-        api.MapGet("enableLogout", OnGetEnableLogout);
-
         return app;
     }
 
-    private static IResult OnGetEnableLogout(HttpContext context)
-    {
-        var header = context.Request.Headers["X-MS-CLIENT-PRINCIPAL-ID"];
-        var enableLogout = !string.IsNullOrEmpty(header);
-
-        return TypedResults.Ok(enableLogout);
-    }
-
-    private static async IAsyncEnumerable<ChatChunkResponse> OnPostChatPromptAsync(
-        PromptRequest prompt,
-        OpenAIClient client,
-        IConfiguration config,
-        [EnumeratorCancellation] CancellationToken cancellationToken)
-    {
-        var deploymentId = config["AZURE_OPENAI_CHATGPT_DEPLOYMENT"];
-        var response = await client.GetChatCompletionsStreamingAsync(
-            new ChatCompletionsOptions
-            {
-                DeploymentName = deploymentId,
-                Messages =
-                {
-                    new ChatRequestSystemMessage("""
-                        You're an AI assistant for developers, helping them write code more efficiently.
-                        You're name is **Blazor 📎 Clippy** and you're an expert Blazor developer.
-                        You're also an expert in ASP.NET Core, C#, TypeScript, and even JavaScript.
-                        You will always reply with a Markdown formatted response.
-                        """),
-                    new ChatRequestUserMessage("What's your name?"),
-                    new ChatRequestAssistantMessage("Hi, my name is **Blazor 📎 Clippy**! Nice to meet you."),
-                    new ChatRequestUserMessage(prompt.Prompt)
-                }
-            }, cancellationToken);
-
-        await foreach (var choice in response.WithCancellation(cancellationToken))
-        {
-            if (choice.ContentUpdate is { Length: > 0 })
-            {
-                yield return new ChatChunkResponse(choice.ContentUpdate.Length, choice.ContentUpdate);
-            }
-        }
-    }
+    
 
     private static async Task<IResult> OnPostChatAsync(
         ChatRequest request,
@@ -140,21 +92,5 @@ internal static class WebApplicationExtensions
         }
     }
 
-    private static async Task<IResult> OnPostImagePromptAsync(
-        PromptRequest prompt,
-        OpenAIClient client,
-        IConfiguration config,
-        CancellationToken cancellationToken)
-    {
-        var result = await client.GetImageGenerationsAsync(new ImageGenerationOptions
-        {
-            Prompt = prompt.Prompt,
-        },
-        cancellationToken);
 
-        var imageUrls = result.Value.Data.Select(i => i.Url).ToList();
-        var response = new ImageResponse(result.Value.Created, imageUrls);
-
-        return TypedResults.Ok(response);
-    }
 }
